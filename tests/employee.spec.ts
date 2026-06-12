31665e89084ac56221c75e978b6f27974cedd46a
Sergio Sánchez Castillo <lecodelsur@gmail.com>
2026-06-12 00:48:16 -0300

    Ejercicio dia 2 Employee name

---
+++ tests/employee.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from "@playwright/test"//importo test de playwright test
+test('Get all the employee names', async ({ page }) => {  //creo mi test y le doy un nombre
+
+    await page.goto('https://opensource-demo.orangehrmlive.com')    //voy a la URl
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin') //en texbox usuario le pido que complete con Admin
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')//en texbox password le pido que complete admin123
+    await page.getByRole('button', { name: 'Login' }).click()//en button login le pido que haga click
+
+    
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible() //aca se espera que cuando se loguee se vea este Admin. 
+
+    await page.getByRole('link', { name: 'Admin' }).click() //aca hago click en Admin de la izquierda. 
+
+    await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('User Management').click()//aca le digo que en la grilla haga click en User Management
+    await page.getByRole('menuitem', { name: 'Users' }).click() //aca le digo que en el menuitem haga click en User para cargar la grilla.
+
+    const rows = page.getByRole('table').getByRole('row') //aca le pido que me guarde en la constante rows 
+    const employeeNames: string[] = [] //aca digo que cree un arreglo y con '= []' lo inicializo a vacio
+
+    const rowCount = await rows.count() //aca quiero contar cuantas filas hay excluyendo el header
+
+    for (let i = 1; i < rowCount; i++) {//que empiece en uno y que sea menor al conteo, y que aumente en cada iteración
+        const cell = rows.nth(i).getByRole('cell').nth(3) //para acceder a la celda específica nth es el indice y se le pone 1 porque quiero la segunda columna
+        const employeename = await cell.textContent() //capture el employee la celda y el metodo que uso es textContent
+
+        if (employeename) {    //textContent puede devolver un nulo por eso uso este if
+            employeeNames.push(employeename)
+        }
+    }
+    console.log(employeeNames) //que imprima todos los usuarios que estan en la inferfaz gráfica.
+})
